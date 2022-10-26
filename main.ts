02cb9847500b9cd923a6cecca70c23fc5c8d9219
marcespii <116082780+marcespii@users.noreply.github.com>
2022-10-26 09:53:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    intermitent(20)
+})
+function intermitent (vegades: number) {
+    for (let index = 0; index < vegades; index++) {
+        led.toggle(2, 2)
+        basic.pause(500)
+    }
+}
+function cor (temps: number) {
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(temps)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(temps)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    cor(10)
+})
+intermitent(10)
+basic.clearScreen()
+basic.pause(500)
+cor(1000)
+basic.clearScreen()
+basic.pause(500)
+basic.showString("BON DIA")
+basic.clearScreen()
+basic.pause(500)
